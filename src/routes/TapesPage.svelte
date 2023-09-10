bf93a5e52ff68379f0ca56da0b3332b6637c29ce
Alex Forsythe <awforsythe@gmail.com>
2023-09-09 20:14:05 -0400

    Fix whitespace in TapesPage.svelte

--- src/routes/TapesPage.svelte
+++ src/routes/TapesPage.svelte
@@ -1,28 +1,28 @@
 <script lang="ts">  
-    import TapeList from '../lib/TapeList.svelte';
-    import { fetchTapes } from '../tapes/index'
-    
-    let promise = fetchTapes()
-  </script>
+  import TapeList from '../lib/TapeList.svelte';
+  import { fetchTapes } from '../tapes/index'
   
-  <main>
-  {#await promise}
-    <p style="text-align: center">Loading...</p>
-  {:then tapes}
-    <TapeList tapes={tapes} />
-  {:catch error}
-    <p>{error.toString()}</p>
-  {/await}
-  </main>
-  
-  <style>
+  let promise = fetchTapes()
+</script>
+
+<main>
+{#await promise}
+  <p style="text-align: center">Loading...</p>
+{:then tapes}
+  <TapeList tapes={tapes} />
+{:catch error}
+  <p>{error.toString()}</p>
+{/await}
+</main>
+
+<style>
+  main {
+    padding: 2rem;
+  }
+  @media only screen and (max-width: 696px) {
     main {
-      padding: 2rem;
+      padding: 1rem;
     }
-    @media only screen and (max-width: 696px) {
-      main {
-        padding: 1rem;
-      }
-    }
-  </style>
+  }
+</style>
   
