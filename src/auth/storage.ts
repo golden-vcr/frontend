26991e06efb86bf1ea0e38ab5d239f79f94b980c
Alex Forsythe <awforsythe@gmail.com>
2023-10-04 23:46:25 -0400

    Serialize role value to/from localStorage

--- src/auth/storage.ts
+++ src/auth/storage.ts
@@ -1,9 +1,12 @@
 import { config } from "../config"
 
-import { type AuthState } from "./types"
+import { type AuthState, type AuthRole } from "./types"
 
 const TWITCH_AUTH_STORAGE_KEY_PREFIX = 'gvcr.twitch.'
 const TWITCH_AUTH_KEY = (name: string) => `${TWITCH_AUTH_STORAGE_KEY_PREFIX}${name}`
+
+const GVCR_AUTH_STORAGE_KEY_PREFIX = 'gvcr.auth.'
+const GVCR_AUTH_KEY = (name: string) => `${GVCR_AUTH_STORAGE_KEY_PREFIX}${name}`
 
 const KEY_TWITCH_CLIENT_ID = TWITCH_AUTH_KEY('clientId')
 const KEY_TWITCH_USER_ID = TWITCH_AUTH_KEY('userId')
@@ -12,6 +15,7 @@ const KEY_TWITCH_USER_DISPLAY_NAME = TWITCH_AUTH_KEY('userDisplayName')
 const KEY_TWITCH_ACCESS_TOKEN = TWITCH_AUTH_KEY('accessToken')
 const KEY_TWITCH_REFRESH_TOKEN = TWITCH_AUTH_KEY('refreshToken')
 const KEY_TWITCH_SCOPES = TWITCH_AUTH_KEY('scopes')
+const KEY_GVCR_ROLE = GVCR_AUTH_KEY('role')
 
 export function cacheAuthState(state: AuthState) {
   if (state.loggedIn) {
@@ -26,6 +30,9 @@ export function cacheAuthState(state: AuthState) {
     localStorage.setItem(KEY_TWITCH_ACCESS_TOKEN, state.tokens.accessToken)
     localStorage.setItem(KEY_TWITCH_REFRESH_TOKEN, state.tokens.refreshToken)
     localStorage.setItem(KEY_TWITCH_SCOPES, state.tokens.scopes.join(' '))
+
+    // Store the Golden VCR role that we've recorded for this user
+    localStorage.setItem(KEY_GVCR_ROLE, state.role)
   } else {
     // If we aren't logged in, zealously clear all auth-related state
     clearAuthState()
@@ -44,6 +51,7 @@ export function restoreAuthState(): AuthState {
   }
 
   // Only continue if we have all required values stored, otherwise clear
+  const role = localStorage.getItem(KEY_GVCR_ROLE) || ''
   const userId = localStorage.getItem(KEY_TWITCH_USER_ID) || ''
   const userLogin = localStorage.getItem(KEY_TWITCH_USER_LOGIN) || ''
   const userDisplayName = localStorage.getItem(KEY_TWITCH_USER_DISPLAY_NAME) || ''
@@ -66,6 +74,7 @@ export function restoreAuthState(): AuthState {
   // we're logged in (note that it's not guaranteed that our tokens are valid)
   return {
     loggedIn: true,
+    role: role as AuthRole,
     user: {
       id: userId,
       login: userLogin,
@@ -84,7 +93,7 @@ export function clearAuthState() {
   const toRemove = [] as string[]
   for (let i = 0; i < localStorage.length; i++) {
     const key = localStorage.key(i) || ''
-    if (key.startsWith(TWITCH_AUTH_STORAGE_KEY_PREFIX)) {
+    if (key.startsWith(TWITCH_AUTH_STORAGE_KEY_PREFIX) || key.startsWith(GVCR_AUTH_STORAGE_KEY_PREFIX)) {
       toRemove.push(key)
     }
   }
