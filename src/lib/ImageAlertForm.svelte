a2c4bdbf1c1ef33cd90ba203dee648dd5180980a
Alex Forsythe <awforsythe@gmail.com>
2023-11-05 15:49:58 -0500

    Add WIP current-tape details and image alert form to homepage

---
+++ src/lib/ImageAlertForm.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+  import { authorizedFetch } from '../auth'
+  import { balance } from '../ledger'
+
+  let subjectText = ''
+  let submissionError = ''
+  let submissionState = 'idle' as 'idle' | 'pending' | 'failed' | 'ok'
+
+  $: numPointsAvailable = $balance.state === 'loaded' ? $balance.value.availablePoints : 0
+
+  async function requestImageGenerationAlert(subject: string) {
+    submissionState = 'pending'
+    const r = await authorizedFetch('/api/showtime/image-gen', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({ subject })
+    })
+
+    if (!r.ok) {
+      let message = ''
+      try {
+        message = await r.text()
+      } catch (ignored) {
+      }
+      const suffix = message ? `: ${message}` : ''
+      submissionState = 'failed'
+      throw new Error(`Got status ${r.status} from POST /image-gen${suffix}`)
+    }
+    submissionState = 'ok'
+  }
+</script>
+
+<fieldset>
+  <p>
+    For only 500 points, you can send a ghostly apparition to haunt my VCR! Simply
+    describe what you'd like to see and click <b>Submit</b>:
+  </p>
+  <input
+    type="text"
+    bind:value={subjectText}
+    disabled={numPointsAvailable < 500}
+  />
+  <button
+    disabled={numPointsAvailable < 500 || submissionState === 'pending' || subjectText.length <= 0 || subjectText.length > 120}
+    on:click={() => requestImageGenerationAlert(subjectText)}
+  >
+    Submit
+  </button>
+{#if submissionState === 'pending'}
+  <p>Submission in progress...</p>
+{:else if submissionState === 'ok'}
+  <p>Submission successful! Your image will appear on the stream shortly.</p>
+{:else if submissionState === 'failed'}
+  <p>Submission failed! No points have been deducted from your balance.</p>
+{/if}
+{#if submissionError}
+  <p><b>ERROR:</b> {submissionError}</p>
+{/if}
+</fieldset>
+
+<style>
+  input {
+    line-height: 2.0;
+    font-size: 1em;
+  }
+</style>
--- src/routes/HomePage.svelte
+++ src/routes/HomePage.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
   import { onMount } from 'svelte'
+  import { Link } from 'svelte-routing'
+
   import { createBroadcastStateSource, type BroadcastState } from '../broadcast'
+  import { tapes } from '../tapes'
+  import { auth } from '../auth'
+  import { balance } from '../ledger'
   import AboutContent from '../lib/AboutContent.svelte'
+  import TapeListItem from '../lib/TapeListItem.svelte'
+  import ImageAlertForm from '../lib/ImageAlertForm.svelte'
 
   let state = { isLive: false } as BroadcastState
   let source = null as EventSource | null
   let errorMessage = ''
+  
+  $: currentTape = (state.isLive && !!state.screeningTapeId) ? ($tapes.tapes.find((x) => state.isLive && x.id === state.screeningTapeId) || null) : null
+  $: numPointsAvailable = $balance.state === 'loaded' ? $balance.value.availablePoints : 0
 
   onMount(() => {
     source = createBroadcastStateSource({
@@ -30,9 +40,48 @@
 </script>
 
 {#if state.isLive}
-  <div>
-    <p>Live now at <a href="https://www.twitch.tv/goldenvcr">twitch.tv/GoldenVCR</a>!</p>
+<div>
+
+{#if currentTape}
+  <p>Join the fun now at <a href="https://www.twitch.tv/goldenvcr">twitch.tv/GoldenVCR</a>, where we're currently watching:</p>
+  <div class="current-tape" style={`border-color: ${currentTape.color}`}>
+    <TapeListItem tape={currentTape} />
   </div>
 {:else}
-  <AboutContent />
+  <p>Live now at <a href="https://www.twitch.tv/goldenvcr">twitch.tv/GoldenVCR</a>!</p>
 {/if}
+
+<p>
+  What are we watching next? You decide! Head on over to the
+  <Link to="/explore">Explore</Link> page to browse the collection.
+</p>
+
+{#if $auth.state.loggedIn}
+<p>
+  Hello, <b>{$auth.state.user.displayName}</b>! You currently have a balance of
+  <b>{numPointsAvailable}</b> Golden VCR Fun Points.
+</p>
+<ImageAlertForm />
+{:else}
+<p>
+  If you'd like to send a ghostly apparition to haunt my VCR, then you can use the
+  <b>Log in with Twitch</b> button above to start using Golden VCR Fun Points.
+  Connecting your Twitch account to GoldenVCR.com just lets us verify your identity as a
+  viewer; we don't require any access to your Twitch account beyond seeing which
+  channels you follow and subscribe to.
+</p>
+{/if}
+
+</div>
+{:else}
+<AboutContent />
+{/if}
+
+<style>
+  .current-tape {
+    border: 1px solid;
+    border-left: 8px solid;
+    padding: 1rem;
+    background-color: #181818;
+  }
+</style>
