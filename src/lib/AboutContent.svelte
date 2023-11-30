0acbe8d6a2580ae0a648b6462f105d55478d3ff9
Alex Forsythe <awforsythe@gmail.com>
2023-11-30 18:24:58 -0500

    Add an extremely lazily-implemented list of links to broadcast pages

--- src/lib/AboutContent.svelte
+++ src/lib/AboutContent.svelte
@@ -1,15 +1,27 @@
 <script lang="ts">
   import { Link } from 'svelte-routing'
+
+  const numBroadcastsToShow = 6
+  const broadcastIds = [...Array(numBroadcastsToShow).keys()].map((x) => x + 1)
 </script>
 
 <p>Follow at: <a href="https://www.twitch.tv/goldenvcr">twitch.tv/GoldenVCR</a></p>
+<p>See the details of past broadcasts:</p>
+<ul>
+{#each broadcastIds as broadcastId}
+  <li>
+    <Link to={`/broadcasts/${broadcastId}`}>Broadcast {broadcastId}</Link>
+  </li>
+{/each}
+</ul>
+
 <p>
   For information about contributing tapes to the Golden VCR Library, click
   <Link to="/contributions">here</Link>.
 </p>
 
 <style>
-  p {
+  p, ul {
     margin-top: 0;
   }
 </style>
