56e7750d2169e5eee6f2e3bbb5a2253099397568
Alex Forsythe <awforsythe@gmail.com>
2023-12-05 14:02:11 -0500

    Add a janky manual link for Broadcast 8

--- src/lib/AboutContent.svelte
+++ src/lib/AboutContent.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { Link } from 'svelte-routing'
 
-  const numBroadcastsToShow = 7
+  const numBroadcastsToShow = 8
   const broadcastIds = [...Array(numBroadcastsToShow).keys()].map((x) => x + 1)
 </script>
 
